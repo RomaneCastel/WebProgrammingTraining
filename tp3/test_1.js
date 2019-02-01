8e54289cce023e82682c7441f238b2017b6922d2
Romane <rcastel@enssat.fr>
2019-02-01 11:31:47 +0100

    same exercise with time control

--- tp3/test_1.js
+++ tp3/test_1.js
@@ -1,19 +1,26 @@
-var square = {x:0, y:0, size:50,};
-var canvas;
-
 function init(){
 	laDiv=document.getElementById('div');
 	//var elem = document.getElementById("square");
-	var left = 0;
+	var left = 500;
 	var timer;
 	// Move the element 10px on the right every 16ms
-	timer = setInterval(function() {
-		laDiv.style.left = ( left += 10 ) + "px";
+	/*timer = setInterval(function() {
+		laDiv.style.left = ( left -= 10 ) + "px";
+		
 		// clear the timer at 400px to stop the animation
-		if ( left > 400 ) {
+		if ( left <=0 ) {
 			clearInterval( timer );
 		}
-	}, 16);
+	},16);*/
+
+	timer = setInterval(function() {
+		laDiv.style.left = ( left -= 10 ) + "px";
+		
+		// clear the timer at 400px to stop the animation
+		if ( left <=0 ) {
+			clearInterval( timer );
+		}
+	},pausecomp(16));
 
 	function pausecomp(millis){
 		var date = new Date();
