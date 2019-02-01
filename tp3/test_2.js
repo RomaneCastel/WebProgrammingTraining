10af21666fc00dfef8ba1a51b63d326fdf1276cc
Romane <rcastel@enssat.fr>
2019-02-01 11:59:37 +0100

    version without function pausecomp

--- tp3/test_2.js
+++ tp3/test_2.js
@@ -1,8 +1,10 @@
 function init(){
 	laDiv=document.getElementById('div');
 	//var elem = document.getElementById("square");
-	var left = 500;
+	var left = 1000;
 	var timer;
+	var lastTime = 0;
+	var currentTime = 0;
 	// Move the element 10px on the right every 16ms
 	/*timer = setInterval(function() {
 		laDiv.style.left = ( left -= 10 ) + "px";
@@ -13,23 +15,21 @@ function init(){
 		}
 	},16);*/
 
-	render();
-	function render(){
-		laDiv.style.left = ( left -= 10 ) + "px";
+	timer = setInterval(function() {
+		laDiv.style.left = ( left -= 200 ) + "px";
+		pausecomp(200);
 		// clear the timer at 400px to stop the animation
 		if ( left <=0 ) {
-			return;
+			clearInterval( timer );
 		}
-		setTimeout(render, 100);
-	}
+	},16);
 
-
-	/*function pausecomp(millis){
+	function pausecomp(millis){
 		var date = new Date();
 		var curDate = null;
 		do{
 			curDate = new Date();
 			//console.log(curDate-date);
 		}while(curDate-date < millis);
-	}*/
+	}
 }
