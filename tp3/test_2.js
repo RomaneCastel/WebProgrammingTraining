b09aeb48536d2d7928a900b2297aa09c8d18ce6e
Romane <rcastel@enssat.fr>
2019-02-01 12:23:14 +0100

    same exercise, red square move, using requestAnimationFrame

--- tp3/test_2.js
+++ tp3/test_2.js
@@ -17,19 +17,18 @@ function init(){
 
 	timer = setInterval(function() {
 		laDiv.style.left = ( left -= 200 ) + "px";
-		pausecomp(200);
 		// clear the timer at 400px to stop the animation
 		if ( left <=0 ) {
 			clearInterval( timer );
 		}
-	},16);
+	},200);
 
-	function pausecomp(millis){
+	/*function pausecomp(millis){
 		var date = new Date();
 		var curDate = null;
 		do{
 			curDate = new Date();
 			//console.log(curDate-date);
 		}while(curDate-date < millis);
-	}
+	}*/
 }
---
+++ tp3/test_3.js
@@ -0,0 +1,22 @@
+function init(){
+	laDiv=document.getElementById('div');
+	var timer;
+	// Move the element 10px on the right every 16ms
+	var timestamp = new Date();
+	var left = 500;
+
+	//render();
+	function render(){
+		//console.
+		var currentTime = new Date();
+		progress = currentTime - timestamp;
+		left -= progress;
+		console.log(left);
+		laDiv.style.left = ( left ) + "px";
+		// clear the timer at 400px to stop the animation
+		window.requestAnimationFrame(render);
+		timestamp = new Date();
+	}
+
+	requestAnimationFrame(render);
+}
